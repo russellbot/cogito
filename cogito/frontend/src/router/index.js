0685e465fa12dda72645ea4642edbda8bcdae97b
Russell Hart <russellbot@gmail.com>
2021-03-17 17:01:13 -0400

    used Vue CLI to create a new View app and connected it to the Django backend

---
+++ cogito/frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Example from "../views/Example.vue";
+import About from "../views/About.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/example",
+    name: "example",
+    component: Example,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  // base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
