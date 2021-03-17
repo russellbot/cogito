b04637b4940ea5e629d00d68394692990356f8f1
Russell Hart <russellbot@gmail.com>
2021-03-17 11:23:21 -0400

    added questions urls, views, and permissions

--- cogito/cogito/urls.py
+++ cogito/cogito/urls.py
@@ -44,6 +44,9 @@ urlpatterns = [
 
     path("api/",
           include("users.api.urls")),
+
+    path("api/",
+          include("questions.api.urls")),
         
     path("api/rest-auth/",
           include("rest_auth.urls")),
